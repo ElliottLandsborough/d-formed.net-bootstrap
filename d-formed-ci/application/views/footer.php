696147ab317d741754cf57520a7f06f752027c97
Elliott Landsborough <elliott@landsborough.co.uk>
2012-05-31 17:05:30 +0100

    tweaks

--- d-formed-ci/application/views/footer.php
+++ d-formed-ci/application/views/footer.php
@@ -20,13 +20,13 @@
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
-  <script src="/js/lightbox.js"></script>
-  <script src="/js/jquery.quovolver.js"></script>
-  <script src="/js/plugins.js"></script>
-  <script src="/js/script.js"></script>
+  <script src="/min/f=js/lightbox.js"></script>
+  <script src="/min/f=js/jquery.quovolver.js"></script>
+  <script src="/min/f=js/plugins.js"></script>
+  <script src="/min/f=js/script.js"></script>
 
   <script>
-    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    var _gaq=[['_setAccount','UA-4886492-1'],['_trackPageview']];
     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
     g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
     s.parentNode.insertBefore(g,s)}(document,'script'));
